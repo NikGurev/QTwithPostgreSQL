ab2fc2c8ee9e3b3243122b4ff5f3abbe590dae9e
kikichik <42917304+kikichik@users.noreply.github.com>
2019-03-02 20:08:41 +0300

    02.03.2019 - added searching
    Now I can find extra symbols from u_LinWU
    problem: I have only one find_table for all records?????

--- SpecSymbols/SpecSymbolsFinder/mainwindow.cpp
+++ SpecSymbols/SpecSymbolsFinder/mainwindow.cpp
@@ -13,14 +13,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->tableView->setModel(model);
     ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
     //вставляем 6 столбцов
-    model->insertColumns(0, 6);
+    model->insertColumns(0, 8);
     model->setHeaderData(0, Qt::Horizontal, "Таблица");
     model->setHeaderData(1, Qt::Horizontal, "Поле");
     model->setHeaderData(2, Qt::Horizontal, "ID(0)");
     model->setHeaderData(3, Qt::Horizontal, "Значение");
     model->setHeaderData(4, Qt::Horizontal, "1й символ");
     model->setHeaderData(5, Qt::Horizontal, "Посл символ");
-    model->setHeaderData(6, Qt::Horizontal, "Таблица поиска")
+    model->setHeaderData(6, Qt::Horizontal, "Таблица поиска");
+    model->setHeaderData(7, Qt::Horizontal, "Id поиска");
 
     int rowItr = 0;
 
@@ -61,7 +62,7 @@ MainWindow::MainWindow(QWidget *parent) :
             QSqlQuery queryGetCols(db);
             queryGetCols.prepare(QString("SELECT attname FROM pg_attribute, pg_type "
                                  "WHERE typname = '%1' AND attrelid = typrelid AND attname NOT IN ('cmin', 'cmax', 'ctid', 'oid', 'tableoid', 'xmin', 'xmax') "
-                                         "AND atttypid=1043").arg(tmpTable));
+                                         "AND atttypid=1043").arg(tmpTable)); //1043 - текстовые поля
 
             if(queryGetCols.exec()){
                 while(queryGetCols.next()){
@@ -86,6 +87,8 @@ MainWindow::MainWindow(QWidget *parent) :
                             //если находим неверный символ в строке, то переходим в таблицу , по которой  производим поиск
                             //ищем поле с нужным названием (как это сделать?) и нужным значением
                             //выводим IdValue строки ,по которой будем осуществлять поиск
+                            //чтобы найти код в таблице, нужно понять, какие есть зависимые таблицы  и от каких зависит таблица
+                            //pg_attrdef.adrelid - таблица к которой принадлежит столбец
                             QString tmpIDvalue = queryFindSpecSymbols.value(tmpID).toString();//Находим ID строки, где некорректный символ
 
                             QChar firstChar = tmpValue.at(0);
@@ -94,14 +97,31 @@ MainWindow::MainWindow(QWidget *parent) :
                             //если последний символ []\^ то проверки нет
                             if(lastChar!='[' && lastChar!=']' && lastChar!='^' && lastChar!='\\'){
                                 qDebug()<<"TABLE: "<<tmpTable<<" FIELD: "<<tmpField<<" ID: "<<tmpID<<" ID_value "<<tmpIDvalue<<" VALUE: "<<tmpValue<<"Char: "<<lastChar;
+                          //Строки, в которые закидываем таблицу и id
+                           QString find_id;
+                           QString find_table="";
+                                if(tmpTable.contains('u')){
+                                QSqlQuery queryFindTable_Id;
 
+                                queryFindTable_Id.prepare("SELECT l_code FROM \"u_LinWU\" WHERE \"l_WU\" = :id ");
+                                queryFindTable_Id.bindValue(0,queryFindSpecSymbols.value(tmpID).toInt());
+                                if(!queryFindTable_Id.exec()){
+                                    qDebug()<<queryFindTable_Id.lastError().text();
+                                }else {
+                                    while(queryFindTable_Id.next())
+                                    find_id = queryFindTable_Id.value(queryFindTable_Id.record().field(0).name()).toString();
+                                    find_table ="u_LinWU";
+                                }
+                            }
                                 model->insertRow(rowItr,QList<QStandardItem*>()<<
                                                  new QStandardItem(tmpTable)<<
                                                  new QStandardItem(tmpField)<<
                                                  new QStandardItem(tmpIDvalue)<<
                                                  new QStandardItem(tmpValue)<<
                                                  new QStandardItem(firstChar)<<
-                                                 new QStandardItem(lastChar)
+                                                 new QStandardItem(lastChar)<<
+                                                 new QStandardItem(find_table)<<
+                                                 new QStandardItem(find_id)
                                                  );
                             }
                         }
